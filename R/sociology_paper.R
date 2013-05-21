c047e3c06bfa01454bb40d751429ca01736d6135
mamajumder <mahbub72@gmail.com>
2013-05-21 17:40:02 -0500

    location effect discussion added

--- R/sociology_paper.R
+++ R/sociology_paper.R
@@ -5,6 +5,7 @@ library(plyr)
 library(reshape)
 library(lme4)
 library(xtable)
+library(grid)
 #library(lubridate)
 
 
@@ -13,6 +14,10 @@ library(xtable)
 raw.dat1 <- read.csv("../data/raw_data_turk1.csv")
 raw.dat2 <- read.csv("../data/raw_data_turk2.csv")
 raw.dat3 <- read.csv("../data/raw_data_turk3.csv")
+raw.dat4 <- read.csv("../data/raw_data_turk4.csv")
+raw.dat5 <- read.csv("../data/raw_data_turk5.csv")
+raw.dat6 <- read.csv("../data/raw_data_turk6.csv")
+raw.dat7 <- read.csv("../data/raw_data_turk7.csv")
 
 source("calculate_ump_power.R") # functions to compute power
 
@@ -38,14 +43,20 @@ clean_data <- function(raw_dat){
   return(subset(cleaned_dat,indx_dup))
 }
 
+remove_duplicated <- function(dat){
+  indx_dup <- with(dat, !duplicated(data.frame(id,pic_id))) #duplication index
+  # now returning cleaned data removing duplication
+  return(subset(dat,indx_dup))
+}
+
 dat1 <- clean_data(raw.dat1)
 dat2 <- clean_data(raw.dat2)
 dat3 <- clean_data(raw.dat3)
-dat4 <- read.csv("../data/raw_data_turk4.csv")
+dat4 <- remove_duplicated(raw.dat4)
 dat4$p_value=0
-dat5 <- read.csv("../data/raw_data_turk5.csv")
-dat6 <- read.csv("../data/raw_data_turk6.csv")
-dat7 <- read.csv("../data/raw_data_turk7.csv")
+dat5 <- remove_duplicated(raw.dat5)
+dat6 <- remove_duplicated(raw.dat6)
+dat7 <- remove_duplicated(raw.dat7)
 
 
 # Summary statistics of the data
@@ -54,7 +65,7 @@ with(dat3, table(age))
 
 
 
-# Examining performance based on plot location in lineup
+# Examining performance based on plot location effect in lineup
 
 dat <- dat2
 
@@ -170,9 +181,9 @@ dtrend <- rbind(data.frame(dtrend1, Experiment = "1"),
 qplot(data=dtrend, attempt, response, geom="point", colour=Experiment) +
   stat_smooth(method="loess")
 
-unique(dtrend$id[dtrend$attempt>45])
+#unique(dtrend$id[dtrend$attempt>45])
 
-subset(dtrend, id==278)
+#subset(dtrend, id==278)
 
 # dtrend1 <- get_trend(dat1)
 qplot(data=dtrend1, attempt, response, geom="point", colour=id) +
@@ -199,8 +210,6 @@ dt1 <- get_smooth_loess(dtrend1)
 qplot(data=smooth_fitted, attempt, fitted, group=id, geom="line")
 
 qplot(data=subset(dt1, id==4), attempt, fitted, group=id, geom="line")
-
-
 
 
 dt1 <- ddply(dtrend1,.(id), summarize,  
@@ -256,14 +265,36 @@ f5 <- fit_model(dtrend5)
 f6 <- fit_model(dtrend6)
 f7 <- fit_model(dtrend7)
 
-dd <- subset(dtrend4, id %in% id[attempt > 9])
-model <- as.formula(response ~ (1|pic_name) + (1|id))
-fit <- lmer(model,family="binomial",data=dd)
+trend.dat <- NULL
+for (i in 5:7){
+  d <- get(paste("dtrend",i,sep=""))
+  dd <- subset(d, id %in% id[attempt > 9])
+  model <- as.formula(response ~ (1|pic_name) + (1|id))
+  fit <- lmer(model,family="binomial",data=dd)
+  dd$resid <- abs(dd$response - fitted(fit))
+  dd$experiment = paste("experiment",i)
+  trend.dat <- rbind(trend.dat ,dd)
+}
 
+ggplot(trend.dat, aes(attempt,resid, colour=experiment))+
+  geom_smooth(method = "loess", size = 1.5) +
+  ylab("Residuals")
+
+ddt <- ddply(trend.dat,.(attempt, experiment), summarise,
+             mean_resid = mean(resid))
+
+qplot(attempt,mean_resid, data= ddt, colour=experiment) +
+  geom_smooth(method="lm") + ylab("Mean absolute residual") +
+  scale_x_continuous(breaks = seq(2,10,by=2))
+
+ggsave("../images/learning_trend.pdf", width=6, height = 4)
+
+dd <- subset(dtrend4, id %in% id[attempt > 9])
+fit <- lmer(model,family="binomial",data=dd)
 dd$resid <- abs(dd$response - fitted(fit))
 qplot(attempt, resid, colour=id, data=subset(dd, id %in% sample(1:125,12)), geom="line", facets=~id)
 qplot(attempt, resid, colour=id, data=dd, geom="line")
-
+qplot(attempt, resid, colour=id, data=dd) + geom_smooth()
 
 
 predict(fit)
@@ -325,6 +356,193 @@ fit3 <- lmer(response ~ factor(attempt) + (attempt|id) + (1|pic_id), family="bin
 anova(fit3,fit2)
 
 
+# some analysis of turk9 data and location effect of the plot
+
+# keep only three responses data. Any responses after the first three are ignored.
+# Also data with less than three responses are discarded considering incomplete.
+
+raw.dat9 <- read.csv("../data/raw_data_turk9.csv", header=T)
+indx_dup <- with(raw.dat9, !duplicated(data.frame(id,pic_id))) #duplication index
+uniq.dat9 <- raw.dat9[indx_dup,] # removing duplicated data if any
+uniq.dat9 <- ddply(uniq.dat9,.(id), transform,
+                  response_order = rank(start_time), 
+                  tot_response=length(response) )
+dat9 <- subset(uniq.dat9, response_order < 4 & tot_response > 2) # filering applied
+dat9$plot_type <- factor(dat9$difficulty, labels=c("Interaction", "Genotype", "Filter"))
+
+qplot(response, geom="bar", data=subset(dat9, plot_type=="Genotype"))
+qplot(response, geom="bar", data=subset(dat9, plot_type=="Filter"))
+qplot(response, geom="bar", data=dat9) + facet_wrap(~difficulty)
+qplot(response, geom="bar", data=dat9) + facet_wrap(~plot_type)
+
+ggsave("../images/performance_plot_type.pdf")
+
+# write.csv(raw.dat9$choice_reason, file= "../data/choice_reason.csv")
+
+with(dat9, table(difficulty))
+with(dat9, table(param_value[plot_type=="Genotype"]))
+with(dat9, table(param_value[plot_type=="Interaction"]))
+
+dd <- ddply(dat9, .(plot_type, plot_location), summarise,
+            percent_correct = mean(response)*100)
+
+ddd <- ddply(subset(dd, plot_type !="Filter"), .(plot_type), summarise,
+             plot_location= c(plot_location, (1:20)[-plot_location]),
+             percent_correct = c(percent_correct, rep(0, 20-length(percent_correct)))) 
+
+ggplot(subset(ddd, plot_type=="Genotype"), aes(1, percent_correct))+
+  geom_bar(stat="identity", fill="darkgreen")+
+  facet_wrap(~plot_location, ncol=5) + 
+  ylab("Proportion of correct evaluation") +
+  xlab("True plot location in the lineup") +
+  theme(axis.text.x = element_blank())
+
+ggsave("../images/location_effect_genotype.pdf")
+
+
+ggplot(subset(ddd, plot_type=="Interaction"), aes(1, percent_correct))+
+  geom_bar(stat="identity", fill="darkgreen")+
+  facet_wrap(~plot_location, ncol=5) + 
+  ylab("Proportion of correct evaluation") +
+  xlab("True plot location in the lineup") +
+  theme(axis.text.x = element_blank())
+
+ggsave("../images/location_effect_Interaction.pdf")
+
+ggplot(subset(dd, plot_type!="Filter")) +
+  geom_bar(aes(factor(plot_location),percent_correct), stat="identity") +
+  facet_wrap(~plot_type)
+
+ggplot(subset(dd, plot_type=="Genotype")) +
+  geom_bar(aes(factor(plot_location),percent_correct), stat="identity") +
+  facet_wrap(~plot_type)
+
+ggplot(subset(dd, plot_type=="Interaction")) +
+  geom_bar(aes(factor(plot_location),percent_correct), stat="identity") +
+  facet_wrap(~plot_type)
+
+
+# proportion of correct evaluation by location and null
+
+nulls <- strsplit(as.character(dat9$param_value),split="_")
+dat9$nulls <- paste("null_",sapply(nulls, function(x) return(x[2])), sep="")
+
+p.dat <- ddply(subset(dat9, plot_type != "Filter"), 
+               .(plot_type, plot_location, nulls),summarise,
+               prop_correct = mean(response), n = length(response))
+m.dat <- ddply(subset(dat9, plot_type != "Filter"), 
+               .(plot_type, plot_location),summarise,
+               prop_correct = mean(response),
+               nulls="Average")
+p.dat$plot_loc <- factor(p.dat$plot_location)
+m.dat$plot_loc <- factor(m.dat$plot_location)
+
+p_prop <- ggplot(p.dat, aes(plot_loc, prop_correct,  group=nulls))+
+  geom_point(aes(color=nulls, size=n)) + 
+  geom_line(aes(color=nulls)) + 
+  facet_wrap(~plot_type, scales = "free_x") + 
+  geom_line(data=m.dat, linetype=5)+
+  xlab("Actual plot location in the lineup")+
+  ylab("Proportion correct") + theme(plot.margin=unit(c(5,1,1,1), "cm"))
+
+ggsave(p_prop,file= "../images/proportion_nulls.pdf", width=9, height=4)
+
+l.dat <- data.frame(plot_location=1:20, plot_int=0, plot_gen=0)
+plot_int <- unique(subset(dat9, plot_type=="Interaction")$plot_location)
+plot_gen <- unique(subset(dat9, plot_type=="Genotype")$plot_location)
+l.dat$plot_int[plot_int] <- 1
+l.dat$plot_gen[plot_gen] <- 1
+p_int <- qplot(1,0, data=l.dat, geom="blank")+ facet_wrap(~plot_location)+ 
+  geom_bar(aes(1,plot_int), stat="identity", alpha=.6) +
+  theme(axis.text = element_blank(), axis.title = element_blank(), plot.margin=unit(c(0,0,-1,-1), "cm")) 
+p_gen <- qplot(1,0, data=l.dat, geom="blank")+ facet_wrap(~plot_location)+ 
+  geom_bar(aes(1,plot_gen), stat="identity", alpha=.6) +
+  theme(axis.text = element_blank(), axis.title = element_blank(), plot.margin=unit(c(0,0,-1,-1), "cm")) 
+
+pushViewport(viewport(layout = grid.layout(2, 2)))  
+print(p_int, vp = viewport(layout.pos.row = 1, layout.pos.col = 1))     
+print(p_gen, vp = viewport(layout.pos.row = 1, layout.pos.col = 2))  
+print(p_prop,vp = viewport(layout.pos.row = 2, layout.pos.col = 1:2))
+
+vp1 <- viewport(x=0.46, y=0.7, height=unit(4.5, "cm"), width=unit(4.5, "cm"), just=c( "right", "bottom"))
+vp2 <- viewport(x=0.86, y=0.7, height=unit(4.5, "cm"), width=unit(4.5, "cm"), just=c( "right", "bottom"))
+
+pdf( "../images/proportion_nulls_guide.pdf", width=11, height=5.9)
+p_prop
+print(p_int,vp = vp1)
+print(p_gen,vp = vp2)
+dev.off()
+
+# Fitting MANOVA model
+df <- ddply(subset(dat9, plot_type != "Filter"), 
+            .(plot_type, plot_location, nulls, pic_name),summarise,
+            prop_correct = mean(response))
+df <- ddply(subset(dat9, plot_type != "Filter"), 
+            .(plot_type, plot_location, nulls, pic_name),transform,
+            replicates = 1:length(response))
+
+# dd <- subset(dat9, plot_type=="Interaction", select=c(response,plot_location, nulls, pic_name))
+
+model.dat <- cast(df,plot_type+plot_location+replicates~nulls, value="response", fun=mean)
+
+model <- as.formula(cbind(null_1,null_2,null_3,null_4,null_5)~factor(plot_location))
+int.dat <- subset(model.dat, plot_type=="Interaction")
+fit1 <- manova(model, data=int.dat )
+summary(fit1)
+
+fit2 <- manova(model, data=subset(model.dat, plot_type=="Genotype") )
+summary(fit2)
+
+
+int.dat$locs <- "out"
+int.dat$locs[int.dat$plot_location %in% c(9,12)] <- "in"
+summary(manova(cbind(null_1,null_2,null_3,null_4,null_5)~factor(locs), data=int.dat))
+
+# push view port
+# test for equivalency
+
+
+
+# Getting visual p-values for turk9 experiment with gene expression data
+
+get_pval <- function(m,K,x){
+  p <- pbinom(size=K,prob=1/m,q=x) # gives P(X<=x)
+  px <- dbinom(size=K,prob=1/m,x=x)
+  pval <- 1-p + px
+  return(pval)
+}
+
+get_response_wt <- function(response_no, plot_location){
+  wt <- NULL
+  for (i in 1:length(plot_location)){
+    response_list <- as.numeric(unlist(strsplit(as.character(response_no[i]),split=",")))
+    wt <- c(wt,(plot_location[i] %in% response_list)/length(response_list))
+  }
+  return(round(sum(wt),0))
+}
+
+pval.dat <- ddply(dat9, .(pic_id), summarise,
+                  plot_type=plot_type[1],
+                  pic_name=pic_name[1],
+                  K = length(response),
+                  x=get_response_wt(response_no, plot_location),
+                  visual_pval = get_pval(m=20, K = length(response),
+                                         x=get_response_wt(response_no, plot_location)))
+# write.csv(pval.dat, file="../data/visual_pval_turk9.csv", row.names=F)
+
+qplot(1,visual_pval, geom="boxplot", data=subset(pval.dat, visual_pval<.05))+
+  facet_wrap(~plot_type)
+
+ddt <- subset(dat9, plot_type=="Interaction")
+get_response_wt(ddt$response_no, ddt$plot_location)
+
+pval.dat <- ddply(dat9, .(plot_type), summarise,
+                  plot_type=plot_type[1],
+                  pic_name=pic_name[1],
+                  K = length(response),
+                  x=get_response_wt(response_no, plot_location),
+                  visual_pval = get_pval(m=20, K = length(response),
+                                         x=get_response_wt(response_no, plot_location)))
 
 # Some exploratory study with the data
 
